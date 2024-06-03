b55cd16af03ba040232228742cb9b06a05eb3290
Ivan <ivanmartinezzs29@gmail.com>
2024-06-03 12:58:58 +0200

    Creando Login Blazor

---
+++ BlazorLogin.Client/Extensiones/AutenticacionExtension.cs
@@ -0,0 +1,20 @@
+﻿using BlazorLogin.Shared;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorLogin.Client.Extensiones
+{
+    public class AutenticacionExtension : AuthenticationStateProvider
+    {
+        private readonly ISessionStorageService _sessionStorage;
+        private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
+
+
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
